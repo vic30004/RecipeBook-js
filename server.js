55e36c69dfa4280dace001b75e0a317b3b0e4e8b
vic30004 <victor_993@msn.com>
2020-02-05 20:18:24 -0800

    set up server

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const app = express();
+const colors = require('colors');
+const PORT = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Server listening on PORT: ${PORT}`.yellow.bold)
+})
