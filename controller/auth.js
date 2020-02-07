447044dfde947e22411a5385d6577caa4d744731
vic30004 <victor_993@msn.com>
2020-02-07 12:30:48 -0800

    working on returning single user

--- controller/auth.js
+++ controller/auth.js
@@ -1,12 +1,12 @@
 const client = require('../config/db');
-const bcrypt = require("bcrypt")
+const bcrypt = require('bcrypt');
 
-exports.register = async(req, res) => {
-    const salt =10;
+exports.register = async (req, res) => {
+  const salt = 10;
   let username = req.body.username;
-  let password =await bcrypt.hash(req.body.password,salt);
+  let password = await bcrypt.hash(req.body.password, salt);
 
-// TODO HASH PASSWORD
+  // TODO HASH PASSWORD
 
   client.query(
     'SELECT username FROM users WHERE username=$1',
@@ -15,14 +15,14 @@ exports.register = async(req, res) => {
       if (err) {
         throw err;
       }
-      if (!user.rows.length<=0) {
-          console.log(user)
-        res.send({user: user.rows,message:"user exists" });
-        console.log(user.rows)
+      if (!user.rows.length <= 0) {
+        console.log(user);
+        res.send({ user: user.rows, message: 'user exists' });
+        console.log(user.rows);
       } else {
         client.query(
           'INSERT into users(username,password) VALUES($1,$2)',
-          [username,password],
+          [username, password],
           (err, user) => {
             if (err) {
               throw err;
@@ -47,5 +47,26 @@ exports.getUsers = (req, res) => {
     });
   } catch (error) {
     console.error(error);
+  }
+};
+
+exports.getSingleUser = (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  let queryStrin = 'SELECT * FROM users WHERE username=$1 AND password=$2';
+  let values = [username, password];
+  console.log(username);
+
+  try {
+    client.query(queryStrin, [username, password], (err, user) => {
+      if (err) {
+        throw err;
+      } else {
+        let data = user.rows;
+        res.status(200).send({ message: "success", user: data });
+      }
+    });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { register,getUsers } = require('../controller/auth');
+const { register,getUsers,getSingleUser } = require('../controller/auth');
 const router = express.Router();
 
 router.post('/', register);
 router.get('/',getUsers)
+router.get('/user',getSingleUser)
 module.exports = router;
