1cf336b6c0ccf95b915c0a6503c5446124422334
vic30004 <victor_993@msn.com>
2020-02-06 23:57:59 -0800

    connected to db

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+const config = require('config');
+const host = config.get('pghost');
+const user = config.get('pguser');
+const password = config.get('pgpassword');
+const port = config.get('pgport');
+const db = config.get('pgdb');
+
+console.log(db);
+const client = new Client({
+  host: host,
+  user: user,
+  password: password,
+  port: port,
+  database: db
+});
+
+module.exports = client;
--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const app = express();
 const colors = require('colors');
+const morgan = require('morgan');
+const path = require('path');
+const dotenv = require('dotenv');
+const client = require('./config/db');
+dotenv.config({ path: './config/config.env' });
+
+const startServer=async()=>{
+  try{
+      await client.connect();
+  console.log('Connected to Database'.blue.bold);
+  }
+  catch(err){
+    console.log(err)
+  }
+
+}
+startServer()
 const PORT = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(morgan('dev'));
 
 
 
 
-app.listen(PORT,(req,res)=>{
-    console.log(`Server listening on PORT: ${PORT}`.yellow.bold)
-})
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server listening on PORT: ${PORT}`.yellow.bold);
+});
