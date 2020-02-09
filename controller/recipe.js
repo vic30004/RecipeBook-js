9108af502bf04f67313d36d66aac1a82c8108954
vic30004 <victor_993@msn.com>
2020-02-08 21:10:04 -0800

    added func to add ingredients

--- controller/recipe.js
+++ controller/recipe.js
@@ -11,7 +11,7 @@ exports.addRecipe = asyncHandler(async (req, res, next) => {
     directions,
     pictureId
   } = req.body;
-console.log(ingredients)
+  console.log(ingredients);
   const id = jwt.verify(req.token, 'dasdfc', (err, authData) => {
     if (err) {
       res.sendStatus(403);
@@ -24,11 +24,37 @@ console.log(ingredients)
   client
     .query(queryString, values)
     .then(data => {
-      res.status(200).send({ message: 'sucess',data:data.rows[0]});
+      addIngredients(data.rows[0].recipe_id, ingredients);
+      client.query(
+        'SELECT user_id,Recipe.recipe_id,title,cook_time,description,directions,pictur_id,date_added,Ingredients.ingredient FROM Recipe INNER JOIN Ingredients ON Ingredients.recipe_id = Recipe.recipe_id WHERE Recipe.recipe_id = $1',
+        [data.rows[0].recipe_id],
+        (err, table) => {
+          if (err) {
+            throw err;
+          } else {
+            res.status(200).send({
+              message: 'sucess',
+              data: table.rows[0]
+            });
+          }
+        }
+      );
     })
     .catch(err => console.log(err.stack));
 });
 
-function addIngredients(id,ingredient){
-    let queryString = 'Insert INTO ingredients(recipe_id,ingredient'
+function addIngredients(id, ingredients) {
+  let stringIngredient = ingredients.join(',');
+  let queryString =
+    'Insert INTO ingredients(recipe_id,ingredient) VALUES($1,$2) RETURNING *';
+  let values = [id, stringIngredient];
+
+  client.query(queryString, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(data.rows[0].ingredient);
+      return data.rows[0].ingredient;
+    }
+  });
 }
