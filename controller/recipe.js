2f406f08b85d7690d48651fc32149d1f44a0e209
vic30004 <victor_993@msn.com>
2020-02-08 20:33:27 -0800

    minor query update

--- controller/recipe.js
+++ controller/recipe.js
@@ -3,12 +3,15 @@ const asyncHandler = require('../middleware/async');
 const jwt = require('jsonwebtoken');
 
 exports.addRecipe = asyncHandler(async (req, res, next) => {
-  const { title, cookTime, description, directions, pictureId } = req.body;
-  const recipeTitle = title;
-  const cTime = cookTime;
-  const desc = description;
-  const direc = directions;
-  const image = pictureId;
+  const {
+    title,
+    cookTime,
+    description,
+    ingredients,
+    directions,
+    pictureId
+  } = req.body;
+console.log(ingredients)
   const id = jwt.verify(req.token, 'dasdfc', (err, authData) => {
     if (err) {
       res.sendStatus(403);
@@ -16,15 +19,16 @@ exports.addRecipe = asyncHandler(async (req, res, next) => {
       return authData.id;
     }
   });
-  const queryString =
-    'INSERT INTO Recipe(user_id,title,cook_time,description,directions) VALUES($1,$2,$3,$4,$5)';
-  const values = [id, recipeTitle, cTime, desc, direc];
-
-  client.query(queryString, values, (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      res.status(200).send('Recipe Added');
-    }
-  });
+  const queryString = `INSERT INTO Recipe(user_id,title,cook_time,description,directions) VALUES($1,$2,$3,$4,$5) RETURNING *`;
+  const values = [id, title, cookTime, description, directions];
+  client
+    .query(queryString, values)
+    .then(data => {
+      res.status(200).send({ message: 'sucess',data:data.rows[0]});
+    })
+    .catch(err => console.log(err.stack));
 });
+
+function addIngredients(id,ingredient){
+    let queryString = 'Insert INTO ingredients(recipe_id,ingredient'
+}
