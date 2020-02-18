1fd451b03ed0558b54e11e5d0cecd3acf4ef5302
vic30004 <victor_993@msn.com>
2020-02-17 16:14:12 -0800

    started styling the cards. Need to change design

--- client/src/pages/Recipes/RecipeItems.js
+++ client/src/pages/Recipes/RecipeItems.js
@@ -5,15 +5,15 @@ const RecipeItems = ({ recipe }) => {
   return <Fragment>{recipe ? 
     <div className="recipe-card">
     {img ?<img className="food-picture" src={img} alt="food-picture"/> :<img src='https://toppng.com/public/uploads/preview/clipart-free-seaweed-clipart-draw-food-placeholder-11562968708qhzooxrjly.png' alt='food-placeholder'></img>}
-    
-    <h3 className="title">{title}</h3>
+    <div className="recipe-content">
+        <h3 className="title">{title}</h3>
     <h4 id="cookTime">Cook Time: {cookTime}</h4>
     {description && (
         <p className="description">{description}</p>
     )}
-    {ingredients.length>0 && ingredients!==null?(
-        <p className="ingredients">Ingredients:{ingredients}</p>
-    ):''}
+    
+    </div>
+
 
     </div> : ''}</Fragment>;
 };
--- client/src/pages/Recipes/Recipes.js
+++ client/src/pages/Recipes/Recipes.js
@@ -20,7 +20,52 @@ export const Recipes = () => {
       ],
       directions:
         "Cook chicken until browned on both sides, make sure to season with salt and pepper. Combine the sauce and the galic and add to chicken. Cook on low heat for another 15 minutes and that's it!"
-    }
+    },
+    {
+        title: 'BBQ Chicken',
+        cookTime: '25 min',
+        description: 'A juicy chicken with BBQ sauce.',
+        img:'https://images.unsplash.com/photo-1475869430886-fb14585f7443?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        ingredients: [
+          '1lb chicken breast',
+          '1cup BBQ sauce',
+          '3 cloves garlic',
+          '2tbs vegetable oil',
+          'salt and peper'
+        ],
+        directions:
+          "Cook chicken until browned on both sides, make sure to season with salt and pepper. Combine the sauce and the galic and add to chicken. Cook on low heat for another 15 minutes and that's it!"
+      },
+      {
+        title: 'BBQ Chicken',
+        cookTime: '25 min',
+        description: 'A juicy chicken with BBQ sauce.',
+        img:'https://images.unsplash.com/photo-1475869430886-fb14585f7443?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        ingredients: [
+          '1lb chicken breast',
+          '1cup BBQ sauce',
+          '3 cloves garlic',
+          '2tbs vegetable oil',
+          'salt and peper'
+        ],
+        directions:
+          "Cook chicken until browned on both sides, make sure to season with salt and pepper. Combine the sauce and the galic and add to chicken. Cook on low heat for another 15 minutes and that's it!"
+      },
+      {
+        title: 'BBQ Chicken',
+        cookTime: '25 min',
+        description: 'A juicy chicken with BBQ sauce.',
+        img:'https://images.unsplash.com/photo-1496074620649-6b1b02e5c1c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        ingredients: [
+          '1lb chicken breast',
+          '1cup BBQ sauce',
+          '3 cloves garlic',
+          '2tbs vegetable oil',
+          'salt and peper'
+        ],
+        directions:
+          "Cook chicken until browned on both sides, make sure to season with salt and pepper. Combine the sauce and the galic and add to chicken. Cook on low heat for another 15 minutes and that's it!"
+      }
   ];
   return (
     <Fragment>
