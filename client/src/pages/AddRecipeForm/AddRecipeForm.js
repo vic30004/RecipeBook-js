453b4dae66edb9c54e5d2ea21ecc33e958d21144
vic30004 <victor_993@msn.com>
2020-03-27 13:49:40 -0700

    can dynamically add input

--- client/src/pages/AddRecipeForm/AddRecipeForm.js
+++ client/src/pages/AddRecipeForm/AddRecipeForm.js
@@ -1,32 +1,96 @@
-import React from 'react'
-import "./AddRecipeForm.css"
+import React, { Fragment, useState } from 'react';
+import './AddRecipeForm.css';
 const AddRecipeForm = () => {
-    return (
-        <div id="recipeForm">
-        <div id="emptyDiv"></div>
-            <div className="form-container">
-                <form action="" className="form-content">
-                    <input type="text" name="title" placeholder="Recipe Title" id="title" required/>
-                    <input type="text" name="cookTime" placeholder="Cook Time" id="cookTime" required/>
-                    <input type="text" name="ingredients" placeholder="Ingredients" id="indgredients" required/>
-                    
-                    <textarea name="" id="" cols="30" rows="4" placeholder="Description"></textarea>
-                    <label htmlFor="">Picture
-                    <input type="file"  name="picture"/>
-            </label>
-            <label htmlFor="">Public
-            <input type="checkbox"/>
-            </label>
-                    
-            <label htmlFor="">Private
-            <input type="checkbox"/>
-            </label>
-                    
-            <button>Add</button>
-                </form>
-            </div>
-        </div>
-    )
+  const [num, setNum] = useState(0);
+//   const dynamicIng =[];
+//   const generateIng = e => {
+
+//     if (e.target.value !== 0 || e.target.value < 20) {
+//       for (let i = 0; i < e.target.value; i++) {
+//     dynamicIng.push(<li>test</li>)
+          
+//     }
+//     console.log(dynamicIng)
+//   };
+// }
+
+const ingInput = (num)=>{
+    const data=[]
+  if(num===0 || num>20){
+      return
+  }
+  for(let i =0; i<num;i++){
+    data.push(<input
+    type='text'
+    name='cookTime'
+    placeholder='Cook Time'
+    id='cookTime'
+    required
+  />)
+}
+ return data
 }
 
-export default AddRecipeForm
+  return (
+    <div id='recipeForm'>
+      <div id='emptyDiv'></div>
+      <div className='form-container'>
+        <form action='' className='form-content'>
+          <h2>Add Recipe</h2>
+          <label>
+            Title* <br></br>
+            <input
+              type='text'
+              name='title'
+              placeholder='Recipe Title'
+              id='title'
+              required
+            />
+          </label>
+          <input type='text' placeholder='0' onChange={e => setNum(e.target.value)} />
+          <label>
+            Cook Time*<br></br>
+            <input
+              type='text'
+              name='cookTime'
+              placeholder='Cook Time'
+              id='cookTime'
+              required
+            />
+          </label>
+          <label>
+            Ingredients*<br></br>
+           {ingInput(num)}
+          </label>
+          <label>
+            Description<br></br>
+            <textarea
+              name=''
+              id=''
+              cols='30'
+              rows='4'
+              placeholder='Description'
+            ></textarea>
+          </label>
+          <label htmlFor=''>Picture</label>
+          <input type='file' name='picture' id='pic' />
+          <div className='check'>
+            <label htmlFor=''>
+              Public
+              <input type='checkbox' />
+            </label>
+
+            <label htmlFor=''>
+              Private
+              <input type='checkbox' />
+            </label>
+          </div>
+
+          <button>Add</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddRecipeForm;
