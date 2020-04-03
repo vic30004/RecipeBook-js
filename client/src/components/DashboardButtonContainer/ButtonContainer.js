a2659a604bf6ab75e60c5f37d9951889ac3a6afb
vic30004 <victor_993@msn.com>
2020-04-03 09:29:53 -0700

    added styling to the button container

--- client/src/components/DashboardButtonContainer/ButtonContainer.js
+++ client/src/components/DashboardButtonContainer/ButtonContainer.js
@@ -1,9 +1,10 @@
 import React,{Fragment} from 'react'
+import styled from 'styled-components';
 
 const ButtonContainer = () => {
     return (
         <Fragment>
-        <div className="button-container">
+        <DashboardBtnContainer>
         <button className="main-container">
           <div>
           <i class="fas fa-carrot fa-4x"></i>
@@ -28,9 +29,23 @@ const ButtonContainer = () => {
               Recipe
           </div>
         </button>
-        </div>
+        </DashboardBtnContainer>
         </Fragment>
     )
 }
 
+const DashboardBtnContainer = styled.section`
+max-width:100%;
+width:800px;
+background: grey;
+margin:0 auto;
+height:300px;
+transform:translateY(-50%);
+border-radius:10px;
+box-shadow: 0 0 10px rgba(0,0,0,0.6);
+`
+
+const DashboardButton = styled.button`
+
+`
 export default ButtonContainer
