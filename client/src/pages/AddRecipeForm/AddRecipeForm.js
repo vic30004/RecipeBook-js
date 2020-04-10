7a1031035f75ab73ce0ce213d8f2cec020e81fab
vic30004 <victor_993@msn.com>
2020-04-10 09:08:42 -0700

    added form data to record the data

--- client/src/pages/AddRecipeForm/AddRecipeForm.js
+++ client/src/pages/AddRecipeForm/AddRecipeForm.js
@@ -1,113 +1,95 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useContext, useEffect } from 'react';
 import './AddRecipeForm.css';
-const AddRecipeForm = () => {
-  const [num, setNum] = useState(0);
-  const [formData,setFormData] = useState()
+import AuthContext from '../../components/context/auth/AuthContext';
 
-  const ingInput = num => {
-    const data = [];
-   
-    for (let i = 0; i < num; i++) {
-      data.push(
-        <input
-          type='text'
-          name='ingredients'
-          placeholder='Ingredients'
-          id='ingredients'
-          key={Math.floor(Math.random()*100)+1}
-          value={formData}
-          onChange={e=>
-            onChange(e)
-          }
-          required
-        />
-      );
+const AddRecipeForm = (props) => {
+  const authContext = useContext(AuthContext);
+  const { loadUser, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (localStorage.token) {
+      loadUser();
+      //eslint-disable-next-line
     }
-    console.log(data);
-    return data;
-  };
-  const addInp = e => {
-    e.preventDefault();
-    setNum(num + 1);
+  }, [isAuthenticated, props]);
 
-    ingInput(num);
-  };
+  const [num, setNum] = useState(0);
+  const [formData, setFormData] = useState({
+    title: '',
+    cookTime: '',
+    directions: '',
+    ingredients: '',
+    description: '',
+    picture: '',
+  });
+  const { title, cookTime, directions, ingredients,description, picture } = formData;
 
-  const onChange = e =>{
-    const ing=e.target.value;
-    setFormData({ ...formData, ...formData.concat(e.target.value)});
-  }
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
   return (
-    <div className="wrapper">
-    
-    
-    <div id='recipeForm'>
-      <div id='emptyDiv'></div>
-      <div className='form-container'>
-        <form action='' className='form-content'>
-          <h2>Add Recipe</h2>
-          <label>
-            Title* <br></br>
-            <input
-              type='text'
-              name='title'
-              placeholder='Recipe Title'
-              id='title'
-              required
-            />
-          </label>
-          <label>
-            Cook Time*<br></br>
-            <input
-              type='text'
-              name='cookTime'
-              placeholder='Cook Time'
-              id='cookTime'
-              required
-            />
-          </label>
-          <label>Ingredients<br></br>
-          <textarea name=''
-          id=''
-          cols='30'
-          rows='4'
-          placeholder='Ingredients'></textarea>
-          
-          </label>
-
-          
-          <label>
-            Description<br></br>
-            <textarea
-              name=''
-              id=''
-              cols='30'
-              rows='4'
-              placeholder='Description'
-            ></textarea>
-          </label>
-          <label htmlFor=''>Picture</label>
-          <input type='file' name='picture' id='pic' />
-          <div className='check-container'>
-            <label className='check-container'>
-              Public
-              <input type='checkbox'/>
-              <span class="checkmark"></span>
-
+    <div className='wrapper'>
+      <div id='recipeForm'>
+        <div id='emptyDiv'></div>
+        <div className='form-container'>
+          <form action='' className='form-content'>
+            <h2>Add Recipe</h2>
+            <label>
+              Title* <br></br>
+              <input
+                type='text'
+                name='title'
+                value={title}
+                placeholder='Recipe Title'
+                id='title'
+                required
+                onChange={(e) => onChange(e)}
+              />
+            </label>
+            <label>
+              Cook Time*<br></br>
+              <input
+                type='text'
+                name='cookTime'
+                placeholder='Cook Time'
+                id='cookTime'
+                value={cookTime}
+                required
+                onChange={(e) => onChange(e)}
+              />
+            </label>
+            <label>
+              Ingredients<br></br>
+              <textarea
+                name='ingredients'
+                id='ingredients'
+                cols='30'
+                rows='4'
+                placeholder='Ingredients'
+                value={ingredients}
+                onChange={(e) => onChange(e)}
+              ></textarea>
             </label>
 
-            <label className='check-container'>
-              Private
-              <input type='checkbox' />
-              <span class="checkmark"></span>
-
+            <label>
+              Description<br></br>
+              <textarea
+                name='description'
+                id='description'
+                cols='30'
+                rows='4'
+                placeholder='Description'
+                value={description}
+                onChange={(e) => onChange(e)}
+              ></textarea>
             </label>
-          </div>
-          <button className="addBtn">Add</button>
-        </form>
+            <label htmlFor=''>Picture</label>
+            <input type='file' name='picture' value={picture} id='pic' onChange={(e) => onChange(e)} />
+
+            <button className='addBtn'>Add</button>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
