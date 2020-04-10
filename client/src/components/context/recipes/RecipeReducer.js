d37ede0d8481982673b478103eaf90c586c2e9dc
vic30004 <victor_993@msn.com>
2020-04-09 20:00:13 -0700

    Added the logic to store the recipes in the state

--- client/src/components/context/recipes/RecipeReducer.js
+++ client/src/components/context/recipes/RecipeReducer.js
@@ -1,5 +1,27 @@
-import { createContext } from 'react';
+import { SHOW_RECIPES, SET_ALERT, REMOVE_ALERT, RECIPE_ERROR } from '../types';
 
-const RecipeContext = createContext();
+export default (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case SHOW_RECIPES:
+      return {
+        ...state,
+        recipes: payload,
+        loading: false,
+      };
 
-export default RecipeContext;
+    case SET_ALERT:
+      return {
+        ...state,
+        errorState: payload,
+      };
+    case REMOVE_ALERT:
+      return {
+        ...state,
+        errorState: [],
+      };
+
+    default:
+      return state;
+  }
+};
