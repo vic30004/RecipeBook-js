4440856382659f3a5078efdf39745731faec8e19
vic30004 <victor_993@msn.com>
2020-04-14 21:14:11 -0700

    started styling the single recipe page

--- client/src/pages/SingleRecipe/SingleRecipe.js
+++ client/src/pages/SingleRecipe/SingleRecipe.js
@@ -18,7 +18,7 @@ const SingleRecipe = ({ match }) => {
     ''
   ) : (
     <div>
-      <h1>{recipe[0].title}</h1>
+      <RecipeTitle>{recipe[0].title}</RecipeTitle>
       <Wrapper>
         <Aside>
           <div className='food-pic'>
@@ -27,7 +27,8 @@ const SingleRecipe = ({ match }) => {
               alt=''
             />
           </div>
-          <div id='ingredients' className='ingredients'>
+                      <div id='ingredients' className='ingredients'>
+                          <h3>Ingredients</h3>
             <ul>
               {test(recipe[0].ingredient, ',').map((data) => (
                 <li key={data}>{data}</li>
@@ -68,36 +69,76 @@ const SingleRecipe = ({ match }) => {
   );
 };
 
-const Wrapper = styled.div`
-display:flex;
-justify-content:center;
+const recipeFont = `font-family: 'Arapey', serif;`;
+const listStyling = (fontSize) => (
+    `      
+list-style-type:none;
+      ${recipeFont};
+      font-size: ${fontSize}rem;
+      margin: 1rem;
+      
+      `
+)
 
+const RecipeTitle = styled.h1`
+margin:0 auto;
+width:1200px;
+padding:1rem;
 `
+
+const Wrapper = styled.div`
+background:ivory;
+display:flex;
+justify-content:space-between;
+border-top: solid 2px #333;
+max-width: 100%;
+width:1200px;
+margin:0 auto;
+/* height:90vh; */
+`;
 const Aside = styled.aside`
+  margin-right: 20px;
+  border-right: solid 2px #333;
+  padding: 2rem;
+  height: 90%;
+  text-align: right;
+
   img {
-    width: 400px;
-    height: 400px;
+    width: 300px;
+    height: 300px;
     border-radius: 0px;
+    margin-bottom: 2rem;
+  }
+
+  ul{
+      margin-bottom:1rem;
+  }
+
+  ul li {
+    ${listStyling(1.5)};
   }
 `;
 const Right = styled.section`
+padding:1rem 3rem;
+transform: translateX(-45%);
 `
 const TextContainer = styled.div`
 
 `
 const Directions = styled.div`
-
-ul li{
-    list-style-type:none;
-}
-
-`
+  ul li {
+    ${listStyling(1.2)};
+  }
+`;
 
 const Title = styled.p`
-font-weight:bold;
+  font-weight: bold;
+  line-height: 4rem;
+  margin-bottom:1rem;
 
-span{
-    font-weight:normal;
-}
-`
+  span {
+    font-weight: normal;
+    ${recipeFont}
+  }
+`;
 export default SingleRecipe;
